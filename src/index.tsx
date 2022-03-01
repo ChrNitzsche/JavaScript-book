81f5ec14b9552071df56f1d3c3649143877a3683
ChrNitzsche <christian.nitzsche83@gmail.com>
2022-03-01 20:05:21 +0100

    implement CodeCompiler & Code-Preview

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import ReactDOM from 'react-dom';
+import { useState, useEffect, useRef } from 'react';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path.plugin';
+import { fetchPlugin } from './plugins/fetch.plugin';
+
+
+const clog = false;
+
+const App = () => {
+   const ref = useRef<any>();
+   const iframe = useRef<any>();
+
+   const [input, setInput] = useState('');
+   const startService = async () => {
+      ref.current = await esbuild.startService({
+         worker: true,
+         wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+      });
+      // console.log(service);
+   };
+
+   useEffect(() => {
+      startService();
+   }, []);
+
+
+   const onClick = async () => {
+      if (!ref.current) {
+         return;
+      }
+                                                                        if (clog) console.log(ref.current);
+
+      iframe.current.srcdoc = html;
+
+      const result = await ref.current.build({
+         entryPoints: ['index.js'],
+         bundle: true,
+         write: false,
+         plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+         define: {
+            'process.env.NODE_ENV': '"production"',
+            global: 'window'
+         }
+      });
+                                                                        if (clog) console.log('result(build) =', result);
+
+      iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+   }
+
+   const html = `<html>
+                  <head></head>
+                  <body>
+                     <div id="root"></div>
+                     <script>
+                        window.addEventListener('message', (event) => {
+                           try {
+                              eval(event.data);
+                           } catch(err) {
+                              const root = document.querySelector('#root');
+                              if(root) root.innerHTML = '<div style="color: red;"><h4>Runtime Error:</h4>' + err + '</div>'
+                              throw err;
+                           }
+                        }, false);
+                     </script>
+                  </body>
+                 </html>`;
+
+   return (
+      <div>
+         <textarea placeholder='Please start coding now...'
+            onChange={ e => setInput(e.target.value) }
+            value={ input }
+         />
+         <div>
+            <button onClick={onClick}>Submit</button>
+         </div>
+         <iframe ref={iframe} sandbox="allow-scripts" srcDoc={ html } title="sandbox" />
+      </div>
+   );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
